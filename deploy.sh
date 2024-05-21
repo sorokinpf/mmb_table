50129b30178c0c795d0c3e575ed296eed9b6cd5e
Pavel Sorokin <you@example.com>
2024-05-21 13:33:27 +0300

    2024v

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 cd build
 zip -r mmb_table.zip *
 scp mmb_table.zip root@mmb-stats.psorokin.ru:~
-ssh root@mmb-stats.psorokin.ru 'cd /var/www/html  && rm -rf * && cp ~/mmb_table.zip .&& unzip mmb_table.zip'
+ssh root@mmb-stats.psorokin.ru 'cd /var/www/html/2024v && rm -rf * && cp ~/mmb_table.zip .&& unzip mmb_table.zip'
 cd ..
